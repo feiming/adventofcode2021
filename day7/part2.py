385e84a04310ebacc9752a46a5924b4d8f8b9450
Fabian Chong <fabian@zyrax.net>
2021-12-07 23:15:14 +0800

    compare

--- day7/part2.py
+++ day7/part2.py
@@ -5,11 +5,12 @@ data = [int(i) for i in data[0].split(",")]
 
 result = {}
 for x in range(2000):
-    print(x)
+    #print(x)
     fuel = 0
     for i in data:
         step = abs(x - i)
         fuel += step + (step*step-1/2)
+        #fuel += step + sum([c for c in range(step)])
     result[x] = fuel
 number = min(result, key=result.get)
 print(result[number])
